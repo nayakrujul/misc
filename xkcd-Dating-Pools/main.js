5f0fe25b83687e18179c6312c1d2140149dc3276
nayakrujul <nayakrujul@hotmail.com>
2024-03-03 14:33:19 +0000

    n/2+7 calculator

---
+++ xkcd-Dating-Pools/main.js
@@ -0,0 +1,17 @@
+const input = document.getElementById("inputbox");
+const outmin = document.getElementById("outputmin");
+const outmax = document.getElementById("outputmax");
+
+function change() {
+    let n = input.value;
+    n = n < 1 ? 1 : n > 120 ? 120: n;
+    input.value = n;
+    if (n < 14) {
+        outmin.innerHTML = outmax.innerHTML = "--";
+    } else {
+        outmin.innerHTML = ("" + (n / 2 + 7)).replaceAll(".5", "&half;");
+        outmax.innerHTML = 2 * (n - 7);
+    }
+}
+
+input.addEventListener("input", change);
