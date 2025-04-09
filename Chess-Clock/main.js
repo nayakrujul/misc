863a539cf4179b769029c3c71fe276050cf2817f
nayakrujul <nayakrujul@hotmail.com>
2025-04-09 21:58:44 +0100

    Update Chess Clock

--- Chess-Clock/main.js
+++ Chess-Clock/main.js
@@ -1,17 +1,41 @@
 const tbs = [...document.getElementsByClassName("time-btn")];
+const min = document.getElementById("minutes");
+const sec = document.getElementById("seconds");
+const inc = document.getElementById("increment");
 
-let times = [6000, 6000];
-let incr = 0;
+let times = [1800, 1800];
+let incr = 20;
 let curr = 0;
 
 let id = -1;
 
 function time_button_clicked(evt) {
+    document.getElementById("custom").open = false;
     tbs.forEach(b => b.classList.remove("selected"));
     evt.target.classList.add("selected");
     let [t, i] = evt.target.value.split(" + ");
+    min.value = t;
+    sec.value = 0;
+    inc.value = i;
     times = [t * 600, t * 600];
     incr = i * 10;
+}
+
+function custom_opened() {
+    if (document.getElementById("custom").open)
+        tbs.forEach(b => b.classList.remove("selected"));
+    else if ([...document.querySelectorAll(".selected")].length == 0)
+        time_button_clicked({target: document.querySelector(`input.time-btn[value="3 + 2"]`)});
+}
+
+function handle_input(evt) {
+    let val = +evt.target.value;
+    evt.target.value = Math.max(val, 0);
+    if (sec.value > 59) {
+        [min.value, sec.value] = divmod(min.value * 60 + +sec.value, 60);
+    }
+    times = [min.value * 600 + sec.value * 10, min.value * 600 + sec.value * 10];
+    incr = inc.value * 10;
 }
 
 function divmod(n, d) {
@@ -76,8 +100,9 @@ function change_turn1() {
 }
 
 tbs.forEach(b => b.addEventListener("click", time_button_clicked));
+document.getElementById("custom").addEventListener("toggle", custom_opened);
+[min, sec, inc].forEach(a => a.addEventListener("input", handle_input));
 document.getElementById("start-btn").addEventListener("click", start);
 
 document.getElementById("clock-div0").addEventListener("click", change_turn0);
 document.getElementById("clock-div1").addEventListener("click", change_turn1);
-
